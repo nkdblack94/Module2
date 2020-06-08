e3f4bd00e76ee67d104e723d86f61f46e4d89ae9
nkdblack94 <nkdblack1294@gmail.com>
2020-06-08 21:36:39 +0700

    BT In ra 20 số nguyên tố dầu tiên

---
+++ src/BT_HienThi_20_SNT_DauTien.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BT_HienThi_20_SNT_DauTien {
+    public static void main(String[] args) {
+        byte number, count = 0, n = 2;
+        boolean snt = true;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bạn muốn in ra bao nhiêu SNT");
+        number = input.nextByte();
+
+        while (count < number){
+            snt = true;
+            for (byte i = 2; i < Math.sqrt(n); i++){
+                if (n % i == 0){
+                    snt = false;
+                    break;
+                }
+            }
+            if (snt){
+                System.out.println(n);
+                count++;
+            }
+            n++;
+        }
+
+    }
+}

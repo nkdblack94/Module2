56247d2d392c8c8effedb2c39818ca7a8a960a95
nkdblack94 <nkdblack1294@gmail.com>
2020-06-08 21:54:34 +0700

    BT hiển thị số nguyên tố nhỏ hơn 100

--- src/BT_HienThi_SNT_NhoHon_100.java
+++ src/BT_HienThi_SNT_NhoHon_100.java
@@ -1,2 +1,22 @@
 public class BT_HienThi_SNT_NhoHon_100 {
+    public static void main(String[] args) {
+        System.out.println("Những SNT nhỏ hơn 100");
+
+        int n = 2;
+        boolean snt = true;
+
+        while (n < 100){
+            snt = true;
+            for (int i = 2; i <= Math.sqrt(n); i++){
+                if (n % i == 0){
+                    snt = false;
+                    break;
+                }
+            }
+            if (snt){
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
 }

e811d9451b02a0204cec6bcaf7dc8bf8e74d8f32
nkdblack94 <nkdblack1294@gmail.com>
2020-06-08 21:22:17 +0700

    BT In các loại hình tam giác

---
+++ src/BT_InCacLoaiHinh.java
@@ -0,0 +1,36 @@
+public class BT_InCacLoaiHinh {
+    public static void main(String[] args) {
+        System.out.println("In hình chữ nhật");
+        for (byte i = 0; i < 3; i++) {
+            System.out.println("* * * * * *");
+        }
+
+        System.out.println("In hình tam giác vuông, có cạnh góc vuông ở botton-left");
+
+        for (byte i = 1; i <= 5; i++) {
+            System.out.println();
+            for (byte j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+        System.out.println("In hình tam giác vuông, có cạnh góc vuông ở top-left");
+
+        for (byte i = 5; i >= 1; i--){
+            System.out.println();
+            for (byte j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+        }
+
+        System.out.println();
+        System.out.println("In hình tam giác cân.");
+
+        for (byte i = 0; i <= 5; i++){
+            System.out.println();
+            for (byte j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+        }
+    }
+}

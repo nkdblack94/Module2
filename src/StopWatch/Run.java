3c6cf606a154b5a8f82a3133ab2399ba954b3bdd
nkdblack94 <nkdblack1294@gmail.com>
2020-06-11 20:39:36 +0700

    [Bài tập] Xây dựng lớp StopWatch

--- src/StopWatch/Run.java
+++ src/StopWatch/Run.java
@@ -1,39 +1,30 @@
 package StopWatch;
 
-import java.util.Scanner;
-
 public class Run {
-    public static void sortSelection(long[] array) {
-        for (long i = 0; i < array.length - 1; i++) {
-            long minIndex = i;
-            for (long j = i + 1; j < array.length; j++) {
-                if (array[(int) j] < array[(int) minIndex]) {
-                    minIndex = j;
+    public static void Sort (long[] arr){
+        long tempSort;
+        for (long i = 0; i < arr.length - 1; i++){
+            for (long j = i + 1; j < arr.length; j++){
+                if (arr[(int) j] < arr[(int) (i)]){
+                    tempSort = arr[(int) j];
+                    arr[(int) i] = arr[(int) j];
+                    arr[(int)j] = tempSort;
                 }
             }
-            swap(array, i, minIndex);
         }
-    }
-    public static void swap(long[] array, long a, long b) {
-        long temp = array[(int) a];
-        array[(int) a] = array[(int) b];
-        array[(int) b] = temp;
     }
 
     public static void main(String[] args) {
 
         long[] arrayNumber = new long[100000];
         for (long i = 0; i < arrayNumber.length; i++) {
-            arrayNumber[(int) i] = (long) Math.floor(Math.random() * 100000);
+            arrayNumber[(int) i] = (long) Math.floor(Math.random() * 1000);
         }
-
 
         StopWatch run = new StopWatch();
         System.out.println("Start time " + run.start());
-        sortSelection(arrayNumber);
+        Sort(arrayNumber);
         System.out.println("Stop time " + run.stop());
         System.out.println("ElapsedTime " + run.getElapsedTime());
-
-
     }
 }

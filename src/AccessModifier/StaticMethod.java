37bebba5c3e405092efb2f9083dcdd1c9be51af6
nkdblack94 <nkdblack1294@gmail.com>
2020-06-12 14:25:16 +0700

    [Thực hành] Static Method

--- src/AccessModifier/StaticMethod.java
+++ src/AccessModifier/StaticMethod.java
@@ -19,19 +19,4 @@ public class StaticMethod {
             System.out.println(number + " " + name + " " + college);
         }
     }
-    public static class TestStaticMethod {
-        public static void main(String[] args) {
-            Student.chang();
-            Student s1 = new Student(1, "Thanh");
-            Student s2 = new Student(2, "Hai");
-            Student s3 = new Student(3, "Nam");
-
-            s1.display();
-            s2.display();
-            s3.display();
-        }
-    }
-
-
-
 }
---
+++ src/AccessModifier/TestStaticMethod.java
@@ -0,0 +1,14 @@
+package AccessModifier;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+        StaticMethod.Student.chang();
+        StaticMethod.Student s1 = new StaticMethod.Student(1, "Thanh");
+        StaticMethod.Student s2 = new StaticMethod.Student(2, "Hai");
+        StaticMethod.Student s3 = new StaticMethod.Student(3, "Nam");
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}

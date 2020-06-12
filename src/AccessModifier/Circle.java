6de3a7b46ead9ca151d7a231582d2da2ce16a64f
nkdblack94 <nkdblack1294@gmail.com>
2020-06-12 14:54:23 +0700

    [Bài tập] Access modifier

---
+++ src/AccessModifier/Circle.java
@@ -0,0 +1,24 @@
+package AccessModifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+   public double getRadius (){
+        return this.radius;
+    }
+
+   public double getArea (){
+        return this.radius * this.radius * Math.PI;
+    }
+
+
+}
